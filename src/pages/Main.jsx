abba0cba84c6d7792f40393c6c3e7634a5b0f207
kim gisu <kgs9843@naver.com>
2025-07-04 20:57:24 +0900

    feat: second test

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,7 +1,7 @@
 const Main = () => {
   return (
     <div className="bg-red-300">
-      <div className="button" style={{ width: "80%" }}>
+      <div className="button" style={{ width: "100%" }}>
         <h1 className="text-red-500 text-3xl font-bold underline">Main</h1>
       </div>
     </div>
